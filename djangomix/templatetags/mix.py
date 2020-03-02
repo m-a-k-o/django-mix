18df14193c65140b3545031e20a796dac7a7e483
Michael Niepel <michael.niepel@gmail.com>
2020-03-02 15:58:26 +0100

    lazy load settings (#7)

--- djangomix/templatetags/mix.py
+++ djangomix/templatetags/mix.py
@@ -5,17 +5,18 @@ from django import template
 
 register = template.Library()
 
-MANIFEST_DIRECTORY = getattr(settings, 'LARAVELMIX_MANIFEST_DIRECTORY','')
+MANIFEST_DIRECTORY = getattr(settings, 'LARAVELMIX_MANIFEST_DIRECTORY', '')
 PUBLIC_URL = getattr(settings, 'LARAVELMIX_PUBLIC_URL', settings.STATIC_URL)
 
+
 @register.simple_tag
-def mix(path, manifest_directory=MANIFEST_DIRECTORY):
-    os_sep = os.path.sep
+def mix(path, manifest_directory=None):
+    if manifest_directory is None:
+        manifest_directory = MANIFEST_DIRECTORY
 
     # laravel-mix generate / on the path in manifest.json
-    if path[0] != '/': # url separator
+    if path[0] != '/':  # url separator
         path = f'/{path}'
-
 
     if os.path.exists(os.path.join(manifest_directory, 'hot')):
         # taken from https://github.com/laravel/framework/blob/master/src/Illuminate/Foundation/Mix.php
@@ -25,11 +26,10 @@ def mix(path, manifest_directory=MANIFEST_DIRECTORY):
         else:
             return f'//localhost:8080{path}'
 
-    manifest_path = os.path.join(manifest_directory ,'mix-manifest.json')
+    manifest_path = os.path.join(manifest_directory, 'mix-manifest.json')
 
     if not os.path.exists(manifest_path):
-        raise FileNotFoundError('Unable to locate manifest file: '
-                                + manifest_path)
+        raise FileNotFoundError('Unable to locate manifest file: ' + manifest_path)
 
     with open(manifest_path, 'r') as f:
         manifest = json.load(f)
